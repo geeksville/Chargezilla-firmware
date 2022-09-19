1cde1083958a663e65c5754ea288fae936afa04c
geeksville <kevinh@geeksville.com>
2022-09-19 11:50:10 -0700

    Nest charging under USB power OK

--- bin/regen-state-graph.sh
+++ bin/regen-state-graph.sh
@@ -1,5 +1,6 @@
 #
-# regenerate the state machine graph from the smcat file
+# regenerate the state machine graph from the smcat file.
+# for smcat syntax documentation see https://github.com/sverweij/state-machine-cat
 # to install:
 # npm install --global state-machine-cat; sudo apt install graphviz
 
